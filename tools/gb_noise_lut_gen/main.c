3e25126620f354e01d9e16e21da3a9798292ab2d
Antonio Niño Díaz <antonio_nd@outlook.com>
2019-11-09 18:31:40 +0000

    Cleanup gb_noise_lut_gen

--- tools/gb_noise_lut_gen/main.c
+++ tools/gb_noise_lut_gen/main.c
@@ -1,43 +1,56 @@
+// SPDX-License-Identifier: MIT
+//
+// Copyright (c) 2014, 2019, Antonio Niño Díaz
 
 #include <stdio.h>
 
 int seed;
-bool exit;
+int done;
 
-int update_lfsr(bool _7bitmode)
+int update_lfsr(int _7bitmode)
 {
 	int out = ((seed & 1) ^ 1);
 	int nextbit = ((seed >> 1) ^ seed) & 1;
 	int shift = _7bitmode ? 6 : 14;
-	seed = (  (seed >> 1) & (~(1<<shift))   )  |  (nextbit<<shift) ;
+	seed = ((seed >> 1) & (~(1 << shift))) | (nextbit << shift);
 	return out;
 }
 
-int getbyte(bool _7bitmode)
+int getbyte(int _7bitmode)
 {
 	int i, value = 0;
-	for(i = 0; i < 8; i++)
-	{
+	for (i = 0; i < 8; i++) {
 		value = (value << 1) | update_lfsr(_7bitmode);
-		if(seed == (_7bitmode ? 0x7F : 0x7FFF)) exit = true;
+		if (seed == (_7bitmode ? 0x7F : 0x7FFF))
+			done = 1;
 	}
 	return value;
 }
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
 	int i;
+	FILE *f;
 
-	i = 0; seed = 0x7F; exit = false;
-	FILE * out = fopen("noise7.txt","wb");
-	while(!exit) fprintf(out,"0x%02x%s",getbyte(true),(((i++)&15) != 15) ? "," : ",\n");
-	fclose(out);
+	i = 0;
+	seed = 0x7F;
+	done = 0;
+	f = fopen("noise7.txt", "wb");
+	while (!done) {
+		fprintf(f, "0x%02x%s", getbyte(1),
+			(((i++) & 15) != 15) ? "," : ",\n");
+	}
+	fclose(f);
 
-	i = 0; seed = 0x7FFF; exit = false;
-	out = fopen("noise15.txt","wb");
-	while(!exit) fprintf(out,"0x%02x%s",getbyte(false),(((i++)&15) != 15) ? "," : ",\n");
-	fclose(out);
+	i = 0;
+	seed = 0x7FFF;
+	done = 0;
+	f = fopen("noise15.txt", "wb");
+	while (!done) {
+		fprintf(f, "0x%02x%s", getbyte(0),
+			(((i++) & 15) != 15) ? "," : ",\n");
+	}
+	fclose(f);
 
 	return 0;
 }
-

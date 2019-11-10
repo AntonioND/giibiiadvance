02f0375b8fcd24dffe7cc767ba191f09100c6490
Antonio Niño Díaz <antonio_nd@outlook.com>
2019-11-10 20:18:05 +0000

    Cleanup png2c

---
+++ tools/png2c/main.c
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+//
+// Copyright (c) 2014, 2019, Antonio Niño Díaz
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "../../png/png_utils.h"
+
+void Debug_LogMsgArg(const char *msg, ...)
+{
+	va_list args;
+	va_start(args, msg);
+	vprintf(msg, args);
+	va_end(args);
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2) {
+		return 1;
+	}
+
+	char *fin = argv[1];
+
+	char *buf;
+	int w, h;
+
+	if (Read_PNG(fin, &buf, &w, &h)) {
+		return 1;
+	}
+
+	FILE *fout = fopen("out.c", "w");
+	if (fout) {
+		int n = 0;
+
+		int i, j;
+		for (j = 0; j < h; j++) {
+			for (i = 0; i < w; i++) {
+				unsigned char r = buf[(j * w + i) * 4 + 0];
+				unsigned char g = buf[(j * w + i) * 4 + 1];
+				unsigned char b = buf[(j * w + i) * 4 + 2];
+				unsigned char a = buf[(j * w + i) * 4 + 3];
+
+				fprintf(fout, "0x%02X,0x%02X,0x%02X,0x%02X,", r,
+					g, b, a);
+
+				n++;
+				if (n == 4) {
+					n = 0;
+					fprintf(fout, "\n");
+				}
+			}
+		}
+
+		fclose(fout);
+	}
+
+	free(buf);
+
+	return 0;
+}

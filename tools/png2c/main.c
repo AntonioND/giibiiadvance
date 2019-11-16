10a0ced0b22f4ef32b3f730c0b7d22fffa6d9320
Antonio Niño Díaz <antonio_nd@outlook.com>
2019-11-16 18:53:05 +0000

    Cleanup png2c
    Fix output.
    
    Clean style.

--- tools/png2c/main.c
+++ tools/png2c/main.c
@@ -7,58 +7,63 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "../png/png_utils.h"
+#include "../../source/png_utils.h"
 
 void Debug_LogMsgArg(const char *msg, ...)
 {
-	va_list args;
-	va_start(args, msg);
-	vprintf(msg, args);
-	va_end(args);
+    va_list args;
+    va_start(args, msg);
+    vprintf(msg, args);
+    va_end(args);
+    printf("\n");
 }
 
 int main(int argc, char *argv[])
 {
-	if (argc < 2) {
-		return 1;
-	}
+    if (argc < 2)
+        return 1;
 
-	char *fin = argv[1];
+    char *fin = argv[1];
 
-	char *buf;
-	int w, h;
+    char *buf;
+    int w, h;
 
-	if (Read_PNG(fin, &buf, &w, &h)) {
-		return 1;
-	}
+    if (Read_PNG(fin, &buf, &w, &h))
+        return 1;
 
-	FILE *fout = fopen("out.c", "w");
-	if (fout) {
-		int n = 0;
+    FILE *fout = fopen("out.c", "w");
+    if (fout) {
+        int n = 0;
 
-		int i, j;
-		for (j = 0; j < h; j++) {
-			for (i = 0; i < w; i++) {
-				unsigned char r = buf[(j * w + i) * 4 + 0];
-				unsigned char g = buf[(j * w + i) * 4 + 1];
-				unsigned char b = buf[(j * w + i) * 4 + 2];
-				unsigned char a = buf[(j * w + i) * 4 + 3];
+        fprintf(fout, "    ");
 
-				fprintf(fout, "0x%02X,0x%02X,0x%02X,0x%02X,", r,
-					g, b, a);
+        for (int j = 0; j < h; j++) {
+            for (int i = 0; i < w; i++) {
+                unsigned char r = buf[(j * w + i) * 4 + 0];
+                unsigned char g = buf[(j * w + i) * 4 + 1];
+                unsigned char b = buf[(j * w + i) * 4 + 2];
+                unsigned char a = buf[(j * w + i) * 4 + 3];
 
-				n++;
-				if (n == 4) {
-					n = 0;
-					fprintf(fout, "\n");
-				}
-			}
-		}
+                fprintf(fout, "0x%02X, 0x%02X, 0x%02X, 0x%02X,",
+                        r, g, b, a);
 
-		fclose(fout);
-	}
+                n++;
+                if (n == 3)
+                {
+                    n = 0;
+                    fprintf(fout, "\n    ");
+                }
+                else
+                {
+                    fprintf(fout, " ");
+                }
+            }
+        }
 
-	free(buf);
+        fclose(fout);
+    }
 
-	return 0;
+    free(buf);
+
+    return 0;
 }

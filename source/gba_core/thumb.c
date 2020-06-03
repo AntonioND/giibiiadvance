186ef67a4fe6b835b2fc57bc82ee3366faa6f34f
Antonio Niño Díaz <antonio_nd@outlook.com>
2020-06-03 18:35:20 +0100

    Don't use GCC extensions in switch() statements

--- source/gba_core/thumb.c
+++ source/gba_core/thumb.c
@@ -296,7 +296,14 @@ s32 GBA_ExecuteTHUMB(s32 clocks)
 
         switch (ident)
         {
-            case 0x0 ... 0x7:
+            case 0x0:
+            case 0x1:
+            case 0x2:
+            case 0x3:
+            case 0x4:
+            case 0x5:
+            case 0x6:
+            case 0x7:
             {
                 // LSL Rd,Rs,#Offset
                 u16 Rd = opcode & 7;
@@ -311,7 +318,14 @@ s32 GBA_ExecuteTHUMB(s32 clocks)
                 // 1S cycle
                 break;
             }
-            case 0x8 ... 0xF:
+            case 0x8:
+            case 0x9:
+            case 0xA:
+            case 0xB:
+            case 0xC:
+            case 0xD:
+            case 0xE:
+            case 0xF:
             {
                 // LSR Rd,Rs,#Offset
                 u16 Rd = opcode & 7;
@@ -326,7 +340,14 @@ s32 GBA_ExecuteTHUMB(s32 clocks)
                 // 1S cycle
                 break;
             }
-            case 0x10 ... 0x17:
+            case 0x10:
+            case 0x11:
+            case 0x12:
+            case 0x13:
+            case 0x14:
+            case 0x15:
+            case 0x16:
+            case 0x17:
             {
                 // ASR Rd,Rs,#Offset
                 u16 Rd = opcode & 7;
@@ -341,7 +362,8 @@ s32 GBA_ExecuteTHUMB(s32 clocks)
                 // 1S cycle
                 break;
             }
-            case 0x18 ... 0x19:
+            case 0x18:
+            case 0x19:
             {
                 // ADD Rd,Rs,Rn
                 u16 Rd = opcode & 7;
@@ -373,7 +395,8 @@ s32 GBA_ExecuteTHUMB(s32 clocks)
                 // 1S cycle
                 break;
             }
-            case 0x1A ... 0x1B:
+            case 0x1A:
+            case 0x1B:
             {
                 // SUB Rd,Rs,Rn
                 u16 Rd = opcode & 7;
@@ -393,7 +416,8 @@ s32 GBA_ExecuteTHUMB(s32 clocks)
                 // 1S cycle
                 break;
             }
-            case 0x1C ... 0x1D:
+            case 0x1C:
+            case 0x1D:
             {
                 // ADD Rd,Rs,#nn
                 u16 Rd = opcode & 7;
@@ -425,7 +449,8 @@ s32 GBA_ExecuteTHUMB(s32 clocks)
                 // 1S cycle
                 break;
             }
-            case 0x1E ... 0x1F:
+            case 0x1E:
+            case 0x1F:
             {
                 // SUB Rd,Rs,#nn
                 u16 Rd = opcode & 7;
@@ -584,7 +609,10 @@ s32 GBA_ExecuteTHUMB(s32 clocks)
             case 0x3F:
                 SUB_REG_IMM(7);
 
-            case 0x40 ... 0x43:
+            case 0x40:
+            case 0x41:
+            case 0x42:
+            case 0x43:
             {
                 // ALU operations
                 u16 Rd = opcode & 7;
@@ -784,7 +812,8 @@ s32 GBA_ExecuteTHUMB(s32 clocks)
             case 0x4F:
                 LDR_REG_PC_IMM(7);
 
-            case 0x50 ... 0x51:
+            case 0x50:
+            case 0x51:
             {
                 // STR  Rd,[Rb,Ro]
                 u16 Rd = opcode & 7;
@@ -796,7 +825,8 @@ s32 GBA_ExecuteTHUMB(s32 clocks)
                           + GBA_MemoryGetAccessCyclesNoSeq32(addr); // 2N
                 break;
             }
-            case 0x52 ... 0x53:
+            case 0x52:
+            case 0x53:
             {
                 // STRH Rd,[Rb,Ro]
                 u16 Rd = opcode & 7;
@@ -808,7 +838,8 @@ s32 GBA_ExecuteTHUMB(s32 clocks)
                           + GBA_MemoryGetAccessCyclesNoSeq16(addr); // 2N
                 break;
             }
-            case 0x54 ... 0x55:
+            case 0x54:
+            case 0x55:
             {
                 // STRB Rd,[Rb,Ro]
                 u16 Rd = opcode & 7;
@@ -820,7 +851,8 @@ s32 GBA_ExecuteTHUMB(s32 clocks)
                           + GBA_MemoryGetAccessCyclesNoSeq16(addr); // 2N
                 break;
             }
-            case 0x56 ... 0x57:
+            case 0x56:
+            case 0x57:
             {
                 // LDSB Rd,[Rb,Ro]
                 u16 Rd = opcode & 7;
@@ -833,7 +865,8 @@ s32 GBA_ExecuteTHUMB(s32 clocks)
                 // 1S+1N+1I
                 break;
             }
-            case 0x58 ... 0x59:
+            case 0x58:
+            case 0x59:
             {
                 // LDR  Rd,[Rb,Ro]
                 u16 Rd = opcode & 7;
@@ -846,7 +879,8 @@ s32 GBA_ExecuteTHUMB(s32 clocks)
                 // 1S+1N+1I
                 break;
             }
-            case 0x5A ... 0x5B:
+            case 0x5A:
+            case 0x5B:
             {
                 // LDRH Rd,[Rb,Ro]
                 u16 Rd = opcode & 7;
@@ -861,7 +895,8 @@ s32 GBA_ExecuteTHUMB(s32 clocks)
                 // 1S+1N+1I
                 break;
             }
-            case 0x5C ... 0x5D:
+            case 0x5C:
+            case 0x5D:
             {
                 // LDRB Rd,[Rb,Ro]
                 u16 Rd = opcode & 7;
@@ -874,7 +909,8 @@ s32 GBA_ExecuteTHUMB(s32 clocks)
                 // 1S+1N+1I
                 break;
             }
-            case 0x5E ... 0x5F:
+            case 0x5E:
+            case 0x5F:
             {
                 // LDSH Rd,[Rb,Ro]
                 u16 Rd = opcode & 7;
@@ -890,7 +926,14 @@ s32 GBA_ExecuteTHUMB(s32 clocks)
                 // 1S+1N+1I
                 break;
             }
-            case 0x60 ... 0x67:
+            case 0x60:
+            case 0x61:
+            case 0x62:
+            case 0x63:
+            case 0x64:
+            case 0x65:
+            case 0x66:
+            case 0x67:
             {
                 // STR  Rd,[Rb,#nn]
                 u16 Rb = (opcode >> 3) & 7;
@@ -902,7 +945,14 @@ s32 GBA_ExecuteTHUMB(s32 clocks)
                           + GBA_MemoryGetAccessCyclesNoSeq32(addr); // 2N
                 break;
             }
-            case 0x68 ... 0x6F:
+            case 0x68:
+            case 0x69:
+            case 0x6A:
+            case 0x6B:
+            case 0x6C:
+            case 0x6D:
+            case 0x6E:
+            case 0x6F:
             {
                 // LDR  Rd,[Rb,#nn]
                 u16 Rb = (opcode >> 3) & 7;
@@ -915,7 +965,14 @@ s32 GBA_ExecuteTHUMB(s32 clocks)
                 // 1S+1N+1I
                 break;
             }
-            case 0x70 ... 0x77:
+            case 0x70:
+            case 0x71:
+            case 0x72:
+            case 0x73:
+            case 0x74:
+            case 0x75:
+            case 0x76:
+            case 0x77:
             {
                 // STRB  Rd,[Rb,#nn]
                 u16 Rb = (opcode >> 3) & 7;
@@ -927,7 +984,14 @@ s32 GBA_ExecuteTHUMB(s32 clocks)
                           + GBA_MemoryGetAccessCyclesNoSeq16(addr); // 2N
                 break;
             }
-            case 0x78 ... 0x7F:
+            case 0x78:
+            case 0x79:
+            case 0x7A:
+            case 0x7B:
+            case 0x7C:
+            case 0x7D:
+            case 0x7E:
+            case 0x7F:
             {
                 // LDRB  Rd,[Rb,#nn]
                 u16 Rb = (opcode >> 3) & 7;
@@ -940,7 +1004,14 @@ s32 GBA_ExecuteTHUMB(s32 clocks)
                 // 1S+1N+1I
                 break;
             }
-            case 0x80 ... 0x87:
+            case 0x80:
+            case 0x81:
+            case 0x82:
+            case 0x83:
+            case 0x84:
+            case 0x85:
+            case 0x86:
+            case 0x87:
             {
                 // STRH  Rd,[Rb,#nn]
                 u16 Rd = opcode & 7;
@@ -952,7 +1023,14 @@ s32 GBA_ExecuteTHUMB(s32 clocks)
                           + GBA_MemoryGetAccessCyclesNoSeq16(addr); // 2N
                 break;
             }
-            case 0x88 ... 0x8F:
+            case 0x88:
+            case 0x89:
+            case 0x8A:
+            case 0x8B:
+            case 0x8C:
+            case 0x8D:
+            case 0x8E:
+            case 0x8F:
             {
                 // LDRH Rd,[Rb,#nn]
                 u16 Rd = opcode & 7;
@@ -1086,7 +1164,9 @@ s32 GBA_ExecuteTHUMB(s32 clocks)
                 // 1S cycle
                 break;
             }
-            case 0xB1 ... 0xB3:
+            case 0xB1:
+            case 0xB2:
+            case 0xB3:
             {
                 // Undefined Opcode #B -- tested in hardware
                 THUMB_UNDEFINED_INSTRUCTION();
@@ -1140,7 +1220,12 @@ s32 GBA_ExecuteTHUMB(s32 clocks)
                 // (n-1)S+2N
                 break;
             }
-            case 0xB6 ... 0xBB:
+            case 0xB6:
+            case 0xB7:
+            case 0xB8:
+            case 0xB9:
+            case 0xBA:
+            case 0xBB:
             {
                 // Undefined opcode #B -- tested on hardware
                 THUMB_UNDEFINED_INSTRUCTION();
@@ -1589,7 +1674,10 @@ s32 GBA_ExecuteTHUMB(s32 clocks)
                 return clocks;
                 //return GBA_ExecuteARM(clocks);
             }
-            case 0xE0 ... 0xE3:
+            case 0xE0:
+            case 0xE1:
+            case 0xE2:
+            case 0xE3:
             {
                 // B label
                 clocks -= GBA_MemoryGetAccessCycles(PCseq, 0, CPU.R[R_PC]);
@@ -1602,7 +1690,10 @@ s32 GBA_ExecuteTHUMB(s32 clocks)
                 CPU.R[R_PC] -= 2;
                 break;
             }
-            case 0xE4 ... 0xE7:
+            case 0xE4:
+            case 0xE5:
+            case 0xE6:
+            case 0xE7:
             {
                 // B label
                 clocks -= GBA_MemoryGetAccessCycles(PCseq, 0, CPU.R[R_PC]);
@@ -1615,13 +1706,23 @@ s32 GBA_ExecuteTHUMB(s32 clocks)
                 CPU.R[R_PC] -= 2;
                 break;
             }
-            case 0xE8 ... 0xEF:
+            case 0xE8:
+            case 0xE9:
+            case 0xEA:
+            case 0xEB:
+            case 0xEC:
+            case 0xED:
+            case 0xEE:
+            case 0xEF:
             {
                 // Undefined Opcode #E -- tested on hardware
                 THUMB_UNDEFINED_INSTRUCTION();
                 break;
             }
-            case 0xF0 ... 0xF3:
+            case 0xF0:
+            case 0xF1:
+            case 0xF2:
+            case 0xF3:
             {
                 // BL label -- First part
                 // LR = PC + 4 + (nn SHL 12)
@@ -1631,7 +1732,10 @@ s32 GBA_ExecuteTHUMB(s32 clocks)
                 // 1S cycle
                 break;
             }
-            case 0xF4 ... 0xF7:
+            case 0xF4:
+            case 0xF5:
+            case 0xF6:
+            case 0xF7:
             {
                 // BL label -- First part
                 // LR = PC + 4 + (nn SHL 12)
@@ -1641,7 +1745,14 @@ s32 GBA_ExecuteTHUMB(s32 clocks)
                 // 1S cycle
                 break;
             }
-            case 0xF8 ... 0xFF:
+            case 0xF8:
+            case 0xF9:
+            case 0xFA:
+            case 0xFB:
+            case 0xFC:
+            case 0xFD:
+            case 0xFE:
+            case 0xFF:
             {
                 // BL label -- Second part
                 // PC = LR + (nn SHL 1), and LR = PC+2 OR 1

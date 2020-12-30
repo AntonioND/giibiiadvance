AntonioND/giibiiadvance

commit 9f1873118d80f006e6750db1af7157d2db75f8f4

Try to prevent rare crashes on emulator exit

source/sound_utils.c
@@ -26,9 +26,11 @@ static SDL_AudioStream *stream;
 
 static void Sound_Callback(unused__ void *userdata, Uint8 *buffer, int len)
 {
+    if (sound_enabled == 0)
+        return;
+
     // Don't play audio during speedup or if it is disabled in the configuration
-    if ((sound_enabled == 0) || EmulatorConfig.snd_mute
-        || Input_Speedup_Enabled())
+    if (EmulatorConfig.snd_mute || Input_Speedup_Enabled())
     {
         // Output silence
         memset(buffer, 0, len);

commit c08b2471e31a35adafec7cc25baf4494d0839cc6

Silence audio when game is paused
Commit 9f1873118d80f006e675 broke this.

source/sound_utils.c
@@ -27,7 +27,10 @@ static SDL_AudioStream *stream;
 static void Sound_Callback(unused__ void *userdata, Uint8 *buffer, int len)
 {
     if (sound_enabled == 0)
+    {
+        memset(buffer, 0, len);
         return;
+    }
 
     // Don't play audio during speedup or if it is disabled in the configuration
     if (EmulatorConfig.snd_mute || Input_Speedup_Enabled())